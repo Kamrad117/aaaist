ea6282e55e1a30181c00669bc6a3ab9b5cba72eb
Dmitriy <Kamrad117@gmail.com>
2012-07-03 20:52:13 +0300

    dep

--- app/models/sponsored.rb
+++ app/models/sponsored.rb
@@ -1,5 +1,5 @@
 class Sponsored < ActiveRecord::Base
-	attr_accessible :name , :description, :immage
+	attr_accessible :name , :description, :immage , :pre_im , :alt_im
 	validates_presence_of :name, :description
  validates_attachment_size :immage, :less_than => 5.megabyte
 	has_attached_file :immage, :styles => { :small =>"150x150>", :large =>"320x240>" , :big =>"800X600>"}
